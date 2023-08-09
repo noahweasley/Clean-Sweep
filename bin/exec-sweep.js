f53fd3090ba2b8ae1f1a8c779529015cf57eaea3
noahweasley <iebenmelu@gmail.com>
2023-08-09 20:43:00 +0100

    Add prompt

--- bin/exec-sweep.js
+++ bin/exec-sweep.js
@@ -1,37 +1,65 @@
 #!/usr/bin/env node
+const path = require("path");
 const cleanSweep = require("../lib/sweep");
-let fileDirectory = process.argv[2];
+const prompt = require("readline").createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+let argv2 = process.argv[2];
 const VALID_PARAMS = ["-v"];
 let params = process.argv.slice(3, process.argv.length);
 
-if (!fileDirectory) {
-  return console.error(
-    "Please specify the target file directory!" +
-      "\n\n" +
-      "Usage: clean-sweep <directory> -v | csw <directory> -v" +
+if (!argv2 || argv2 === "-h") {
+  prompt.close();
+  return console.info(
+    "Usage: clean-sweep <directory> -v | csw <directory> -v | csw -h | clean-sweep -h" +
       "\n\n" +
       "Parameters:" +
       " \n\n" +
-      " -v: Prints operation to console"
+      " -v: Prints operation to console" +
+      " \n\n" +
+      " -h: Shows command usage"
   );
 } else if (!params.every((param) => VALID_PARAMS.includes(param))) {
-  return console.error(
-    "Invalid parameter!" +
-      "\n\n" +
-      "Valid parameters:" +
-      " \n\n" +
-      " -v: Prints operation to console"
-  );
+  prompt.close();
+  if (params.includes("-h")) {
+    return console.info(
+      "Please use csw -h or clean-sweep -h to display command usage"
+    );
+  } else {
+    return console.error(
+      "Invalid parameter!" +
+        "\n\n" +
+        "Valid parameters:" +
+        " \n\n" +
+        " -v: Prints operation to console" +
+        " \n\n" +
+        " -h: Shows command usage"
+    );
+  }
 } else {
   try {
-    if (params.includes("-v")) {
-      cleanSweep.sweep(fileDirectory, { verbose: true });
-    } else {
-      cleanSweep.sweep(fileDirectory);
-    }
+    prompt.question(
+      `Delete all OB files in "${path.resolve(argv2)}"?\nYes or No: `,
+      function (answer) {
+        if (["yes", "y"].includes(answer.toLowerCase())) {
+          if (params.includes("-v")) {
+            cleanSweep.sweep(argv2, { verbose: true });
+          } else {
+            cleanSweep.sweep(argv2);
+          }
+          prompt.close();
+        } else {
+          prompt.close();
+          return;
+        }
+      }
+    );
   } catch (err) {
+    prompt.close();
     if (err.code == "ENOENT") {
-      console.error(`Directory "${fileDirectory}" does not exist`);
+      console.error(`Directory "${argv2}" does not exist`);
     } else {
       console.error(err.message);
     }
